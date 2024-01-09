c7be2e8431d29bb439c9cee157396b4d24588167
Eric Ubaga <ubaga91@gmail.com>
2024-01-09 18:18:41 +0300

    Changed application to deep search first algorithm

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,20 +7,38 @@ def canUnlockAll(boxes):
     Function  that determines if all the boxes can be opened.
     Returns True or False
     """
-    can_unlock = {0}
-    all_boxes = set(range(1, len(boxes)))
+    if type(boxes) != list:
+        raise TypeError("boxes must be a list")
+    # can_unlock = {0}
+    # all_boxes = set(range(1, len(boxes)))
 
-    if len(boxes) > 0:
-        # Keep track of newly opened boxes
-        new_boxes = {0}
+    # if len(boxes) > 0:
+    #     # Keep track of newly opened boxes
+    #     new_boxes = {0}
 
-        while new_boxes:
-            # Reset new_boxes for the next round of keys
-            next_keys = set()
-            for i in new_boxes:
-                next_keys.update(boxes[i])
-            new_boxes = next_keys.difference(can_unlock)
-            can_unlock.update(next_keys)
-            all_boxes.difference_update(can_unlock)
+    #     while new_boxes:
+    #         # Reset new_boxes for the next round of keys
+    #         next_keys = set()
+    #         for i in new_boxes:
+    #             if type(boxes[i]) != list:
+    #                 raise TypeError('boxes must be a list of lists')
+    #             next_keys.update(boxes[i])
+    #         new_boxes = next_keys.difference(can_unlock)
+    #         can_unlock.update(next_keys)
+    #         all_boxes.difference_update(can_unlock)
 
-    return len(all_boxes) == 0
+    # return len(all_boxes) == 0
+    visited = set()
+    # use deep search first algorithm
+
+    def dfs(box):
+        if box in visited:
+            return
+        visited.add(box)
+        for key in boxes[box]:
+            print('key is {}'.format(key))
+            dfs(key)
+
+    dfs(0)  # Start DFS from box 0
+
+    return len(visited) == len(boxes)
