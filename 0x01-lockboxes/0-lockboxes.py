c7d9fa76cce28a60a590e383a1fe00f9e89d74c1
Eric Ubaga <ubaga91@gmail.com>
2024-01-09 21:45:08 +0300

    Added checks for boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,31 +7,15 @@ def canUnlockAll(boxes):
     Function  that determines if all the boxes can be opened.
     Returns True or False
     """
-    if type(boxes) != list:
-        raise TypeError("boxes must be a list")
-    # can_unlock = {0}
-    # all_boxes = set(range(1, len(boxes)))
+    if type(boxes) != list or len(boxes) == 0:
+        return False
 
-    # if len(boxes) > 0:
-    #     # Keep track of newly opened boxes
-    #     new_boxes = {0}
-
-    #     while new_boxes:
-    #         # Reset new_boxes for the next round of keys
-    #         next_keys = set()
-    #         for i in new_boxes:
-    #             if type(boxes[i]) != list:
-    #                 raise TypeError('boxes must be a list of lists')
-    #             next_keys.update(boxes[i])
-    #         new_boxes = next_keys.difference(can_unlock)
-    #         can_unlock.update(next_keys)
-    #         all_boxes.difference_update(can_unlock)
-
-    # return len(all_boxes) == 0
     visited = set()
     # use deep search first algorithm
 
     def dfs(box):
+        if type(boxes[box]) != list:
+            raise TypeError("boxes must be a list of list")
         if box in visited:
             return
         visited.add(box)
