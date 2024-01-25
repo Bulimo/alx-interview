ddcecd412d5a90657cc420f7dbc605eecc87f456
Eric Ubaga <ubaga91@gmail.com>
2024-01-25 21:50:44 +0300

    Check input using regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,6 +5,7 @@
 import sys
 from collections import defaultdict
 import signal
+import re
 
 
 def print_statistics(total_size, status_counts):
@@ -13,6 +14,16 @@ def print_statistics(total_size, status_counts):
     for code in sorted(status_counts):
         print(f"{code}: {status_counts[code]}")
     sys.stdout.flush()
+
+
+def check_input(line):
+    """checks if the line format is valid """
+    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+    r'\[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+    match = re.match(pattern, line)
+    if match:
+        return True
+    return False
 
 
 def main():
@@ -33,20 +44,26 @@ def main():
             line_count += 1
             # Split the line by spaces
             # print("line = {}".format(line))
+            line.strip()
+            if not check_input(line):
+                continue
             parts = line.split()
             # print("parts = {}".format(parts))
             # Check if the line has the expected format
-            if len(parts) == 9 and parts[0].count(".") == 3\
-                    and parts[4].startswith("\"GET")\
-                    and parts[5] == '/projects/260'\
-                    and parts[6].endswith("HTTP/1.1\"") and parts[7].isdigit()\
-                    and parts[8].isdigit():
-                # Extract the status code and file size
-                # print("Extracting the data")
+            # if len(parts) == 9 and parts[0].count(".") == 3\
+            #         and parts[4] == ("\"GET")\
+            #         and parts[5] == '/projects/260'\
+            #         and parts[6] == ("HTTP/1.1\"") and parts[7].isdigit()\
+            #         and parts[8].isdigit():
+            # Extract the status code and file size
+            # print("Extracting the data")
+            try:
                 status_code = int(parts[7])
                 file_size = int(parts[8])
                 total_size += file_size
                 status_counts[status_code] += 1
+            except Exception:
+                pass
 
             if line_count % 10 == 0:
                 print_statistics(total_size, status_counts)
