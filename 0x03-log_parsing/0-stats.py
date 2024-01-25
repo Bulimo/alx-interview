bfe9145cc4b975678038bc453e2cdce8c44b6d64
Eric Ubaga <ubaga91@gmail.com>
2024-01-25 23:06:47 +0300

    fix task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,14 +15,14 @@ def print_statistics(total_size, status_counts):
     sys.stdout.flush()
 
 
-def check_input(line):
-    """checks if the line format is valid """
-    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
-    r'\[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-    match = re.match(pattern, line)
-    if match:
-        return True
-    return False
+# def check_input(line):
+#     """checks if the line format is valid """
+#     pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+#     r'\[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+#     match = re.match(pattern, line)
+#     if match:
+#         return True
+#     return False
 
 
 def main():
@@ -43,28 +43,28 @@ def main():
             parts = line.split()
             # print("parts = {}".format(parts))
             # Check if the line has the expected format
-            if parts[0].count(".") == 3\
-                    and parts[4] == ("\"GET")\
-                    and parts[5] == '/projects/260'\
-                    and parts[6] == ("HTTP/1.1\"") and parts[7].isdigit()\
-                    and parts[8].isdigit():
-                # Extract the status code and file size
-                # print("Extracting the data")
-                try:
-                    status_code = int(parts[7])
-                    if status_code in codes:
-                        if status_code not in status_counts:
-                            status_counts[status_code] = 1
-                        else:
-                            status_counts[status_code] += 1
-                except Exception as e:
-                    continue
+            # if parts[0].count(".") == 3\
+            #         and parts[4] == ("\"GET")\
+            #         and parts[5] == '/projects/260'\
+            #         and parts[6] == ("HTTP/1.1\"") and parts[7].isdigit()\
+            #         and parts[8].isdigit():
+            # Extract the status code and file size
+            # print("Extracting the data")
+            try:
+                status_code = int(parts[7])
+                if status_code in codes:
+                    if status_code not in status_counts:
+                        status_counts[status_code] = 1
+                    else:
+                        status_counts[status_code] += 1
+            except Exception as e:
+                continue
 
-                try:
-                    file_size = int(parts[8])
-                    total_size += file_size
-                except Exception as e:
-                    continue
+            try:
+                file_size = int(parts[8])
+                total_size += file_size
+            except Exception as e:
+                continue
 
             if line_count % 10 == 0:
                 print_statistics(total_size, status_counts)
