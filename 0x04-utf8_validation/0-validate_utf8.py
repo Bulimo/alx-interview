72674b81e3d8ec3de1287cced952d524a0c6949f
Eric Ubaga <ubaga91@gmail.com>
2024-02-01 18:01:43 +0300

    Reverted to the previous implementation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -32,16 +32,16 @@ def validUTF8(data):
     # Iterate over all the characters in the data set
     for xter in data:
         val = dec_to_bin(xter)
-        # if 1 not in val:
-        #     # print("False coz all 8 bits being 0")
-        #     return False
-        if val:  # continuation_bits > 0:
-            #     # This character is part of the previous data set
-            #     if val[0] != 1 and val[1] != 0:
-            #         # print("False coz continuation is not [10xxxx]")
-            #         return False
-            #     continuation_bits -= 1
-            # else:
+        if 1 not in val:
+            # print("False coz all 8 bits being 0")
+            return False
+        if continuation_bits > 0:
+            # This character is part of the previous data set
+            if val[0] != 1 and val[1] != 0:
+                # print("False coz continuation is not [10xxxx]")
+                return False
+            continuation_bits -= 1
+        else:
             # Start of new character set of data
             # print("val = {}".format(val))
             # Check for invalid starting patterns
